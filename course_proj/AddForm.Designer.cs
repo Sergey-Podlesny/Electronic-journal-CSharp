70273136415877760fc59375fedcc14f49af9d99
Sergey Podlesny <srgpdlsn@gmail.com>
2021-02-26 21:29:15 +0300

    push project

---
+++ course_proj/AddForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace kursach
+{
+    partial class AddForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.surnameTextBox = new System.Windows.Forms.TextBox();
+            this.markTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(60, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Номер:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(41, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Фамилия:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(43, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Отметки:";
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numberTextBox.Location = new System.Drawing.Point(141, 33);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(152, 30);
+            this.numberTextBox.TabIndex = 0;
+            this.numberTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.numberTextBox_KeyPress);
+            // 
+            // surnameTextBox
+            // 
+            this.surnameTextBox.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.surnameTextBox.Location = new System.Drawing.Point(141, 69);
+            this.surnameTextBox.Name = "surnameTextBox";
+            this.surnameTextBox.Size = new System.Drawing.Size(152, 30);
+            this.surnameTextBox.TabIndex = 1;
+            // 
+            // markTextBox
+            // 
+            this.markTextBox.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.markTextBox.Location = new System.Drawing.Point(141, 105);
+            this.markTextBox.Name = "markTextBox";
+            this.markTextBox.Size = new System.Drawing.Size(152, 30);
+            this.markTextBox.TabIndex = 2;
+            this.markTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.markTextBox_KeyPress);
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.addButton.Location = new System.Drawing.Point(141, 145);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(152, 64);
+            this.addButton.TabIndex = 3;
+            this.addButton.Text = "Добавить";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // AddForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 242);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.markTextBox);
+            this.Controls.Add(this.surnameTextBox);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AddForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Добавление";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.TextBox surnameTextBox;
+        private System.Windows.Forms.TextBox markTextBox;
+        private System.Windows.Forms.Button addButton;
+    }
+}
---
+++ course_proj/AddForm.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kursach
+{
+    public partial class AddForm : Form
+    {
+
+        List<Student> students;
+        string lesson, form;
+        string[] marksString;
+
+        public AddForm(ref List<Student> stud, string less, string _form)
+        {
+            InitializeComponent();
+            lesson = less;
+            students = stud;
+            form = _form;
+        }
+
+        public bool PreviousIsDigit(char curLetter)
+        {
+            bool isDigit = false;
+            if (markTextBox.Text.Length > 0 && char.IsDigit(markTextBox.Text[markTextBox.Text.Length - 1]))
+            {
+                isDigit = true;
+            }
+            if (markTextBox.Text.Length > 0 && markTextBox.Text[markTextBox.Text.Length - 1] == '1' && curLetter == '0')
+            {
+                isDigit = false;
+            }
+            return isDigit;
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            if (numberTextBox.Text.Trim() != "" && surnameTextBox.Text.Trim() != "")
+            {
+                if (IndexChecking())
+                {
+                    FillList();
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Вы ввели номер, который уже занят.", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Вы не заполнили поля НОМЕР и ФАМИЛИЯ.", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void FillList()
+        {
+            int temp;
+            Student tempStudent = new Student();
+            tempStudent.form = form;
+            int.TryParse(numberTextBox.Text.Trim(), out temp);
+            tempStudent.id = temp;
+            tempStudent.surname = surnameTextBox.Text.Trim();
+            marksString = markTextBox.Text.Trim().Split(' ');
+            int[] tempArray;
+            switch (lesson)
+            {
+                case "Физика":
+                    if (markTextBox.Text == "")
+                    {
+                        tempStudent.phisMarks = null;
+                    }
+                    else
+                    {
+                        tempArray = tempStudent.phisMarks;
+                        FillMarks(ref tempArray);
+                        tempStudent.phisMarks = tempArray;
+                    }
+                    break;
+                case "Математика":
+                    if (markTextBox.Text == "")
+                    {
+                        tempStudent.mathMarks = null;
+                    }
+                    else
+                    {
+                        tempArray = tempStudent.mathMarks;
+                        FillMarks(ref tempArray);
+                        tempStudent.mathMarks = tempArray;
+                    }
+                    break;
+                case "Русский язык":
+                    if (markTextBox.Text == "")
+                    {
+                        tempStudent.langMarks = null;
+                    }
+                    else
+                    {
+                        tempArray = tempStudent.langMarks;
+                        FillMarks(ref tempArray);
+                        tempStudent.langMarks = tempArray;
+                    }
+                    break;
+            }
+
+
+            students.Add(tempStudent);
+        }
+
+        public bool IndexChecking()
+        {
+            bool isNotEmpty = true;
+            for (int i = 0; i < students.Count; i++)
+            {
+                if (students[i].form == form && students[i].id.ToString() == numberTextBox.Text)
+                {
+                    isNotEmpty = false;
+                    break;
+                }
+            }
+            return isNotEmpty;
+        }
+
+        private void numberTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void markTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((!char.IsDigit(e.KeyChar) || PreviousIsDigit(e.KeyChar)) && e.KeyChar != 8 && e.KeyChar != 32)
+            {
+                e.Handled = true;
+            }
+        }
+
+        public void FillMarks(ref int[] marksInt)
+        {
+            marksInt = new int[marksString.Length];
+            for (int i = 0; i < marksString.Length; i++)
+            {
+                int tempMark;
+                int.TryParse(marksString[i], out tempMark);
+                marksInt[i] = tempMark;
+            }
+        }
+    }
+}
