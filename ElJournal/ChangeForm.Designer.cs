5b32380e8fbcfa8ee2279b99c4f56fd6bbfb5c0d
Sergey Podlesny <srgpdlsn@gmail.com>
2021-02-27 01:29:56 +0300

    Rename

---
+++ ElJournal/ChangeForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace kursach
+{
+    partial class ChangeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.markTextBox = new System.Windows.Forms.TextBox();
+            this.surnameTextBox = new System.Windows.Forms.TextBox();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // markTextBox
+            // 
+            this.markTextBox.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.markTextBox.Location = new System.Drawing.Point(141, 105);
+            this.markTextBox.Name = "markTextBox";
+            this.markTextBox.Size = new System.Drawing.Size(152, 30);
+            this.markTextBox.TabIndex = 11;
+            this.markTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.markTextBox_KeyPress);
+            // 
+            // surnameTextBox
+            // 
+            this.surnameTextBox.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.surnameTextBox.Location = new System.Drawing.Point(141, 69);
+            this.surnameTextBox.Name = "surnameTextBox";
+            this.surnameTextBox.Size = new System.Drawing.Size(152, 30);
+            this.surnameTextBox.TabIndex = 10;
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numberTextBox.Location = new System.Drawing.Point(141, 33);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(152, 30);
+            this.numberTextBox.TabIndex = 9;
+            this.numberTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.numberTextBox_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(43, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 24);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Отметки:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(41, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Фамилия:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(60, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Номер:";
+            // 
+            // changeButton
+            // 
+            this.changeButton.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.changeButton.Location = new System.Drawing.Point(141, 145);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(152, 64);
+            this.changeButton.TabIndex = 12;
+            this.changeButton.Text = "Изменить";
+            this.changeButton.UseVisualStyleBackColor = true;
+            this.changeButton.Click += new System.EventHandler(this.changeButton_Click);
+            // 
+            // ChangeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 242);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.markTextBox);
+            this.Controls.Add(this.surnameTextBox);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "ChangeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Изменение";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox markTextBox;
+        private System.Windows.Forms.TextBox surnameTextBox;
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button changeButton;
+    }
+}
---
+++ ElJournal/DeleteForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kursach
+{
+    public partial class DeleteForm : Form
+    {
+        List<Student> students;
+        string form;
+        public DeleteForm(ref List<Student> stud, string _form)
+        {
+            InitializeComponent();
+            students = stud;
+            form = _form;
+        }
+
+        public bool IndexChecking()
+        {
+            bool isNotEmpty = false;
+            for (int i = 0; i < students.Count; i++)
+            {
+                if (students[i].form == form && students[i].id.ToString() == numberTextBox.Text)
+                {
+                    isNotEmpty = true;
+                    break;
+                }
+            }
+            return isNotEmpty;
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (IndexChecking())
+            {
+                int num;
+                int.TryParse(numberTextBox.Text, out num);
+                for (int i = 0; i < students.Count; i++)
+                {
+                    if (students[i].id == num && students[i].form == form)
+                    {
+                        students.RemoveAt(i);
+                        i--;
+                    }
+                }
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Вы ввели номер несуществующий строки.", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void numberTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
